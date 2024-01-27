2bf99899c276addecea8da325984f3fb255039e5
Jason Gates <95332717+JasonDGates@users.noreply.github.com>
2024-01-26 21:00:50 -0600

    FEAT: Setting up express server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,24 @@
-const express = require('express');
+// const express = require('express');
+// const path = require('path');
+// const cors = require('cors');
+// const corsOptions = require('./config/corsOptions');
+
+import express from 'express';
+
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(201).send('Hello World');
+})
+
+// app.get('/api/users', (req, res) => {
+//     const {
+//         query: {filter, value}
+//     } = request;
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
