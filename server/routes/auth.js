e8cd79c4f4f7a0ad0bc7744a675332604b31d5cf
Jason <gatesjasond@gmail.com>
2024-06-05 22:48:19 -0500

    FEAT: Added logout route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,4 +8,12 @@ router.post("/auth", passport.authenticate("local"), (req, res) => {
   res.sendStatus(200);
 });
 
+router.post("/auth/logout", (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  req.logout((error) => {
+    if (error) return res.sendStatus(400);
+    res.sendStatus(200);
+  });
+});
+
 export default router;
