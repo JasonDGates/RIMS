58098a0b8bc328c451be7454654a64ab2e4b830f
Jason <gatesjasond@gmail.com>
2024-07-23 23:41:24 -0500

    Updating Sign Up Form

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const LoginComponent = ({ onSignUpClick }) => (
   <div>
@@ -11,13 +11,37 @@ const LoginComponent = ({ onSignUpClick }) => (
 );
 
 const SignupComponent = ({ onSignInClick }) => (
-  <div>
-    <h2>Sign Up</h2>
-    <input type="email" placeholder="Email Address" />
-    <input type="password" placeholder="Password" />
-    <input type="password" placeholder="Verify Password" />
-    <button>Sign Up</button>
-    <p onClick={onSignInClick}>Already have an account? Click here</p>
+  <div class="flex items-center flex-col h-screen">
+    <div class="flex flex-col items-center max-w-[500px]">
+      <h2 class="m-2">Sign In</h2>
+      <div class="flex w-full">
+        <div>
+          <input
+            type="first-name"
+            placeholder="First Name"
+            class="border m-2 mb-0 p-2 rounded-lg"
+          />
+          <p class="text-red-500 text-xs italic h-4 ml-2"></p>
+        </div>
+        <div>
+          <input
+            type="last-name"
+            placeholder="Last Name"
+            class="border m-2 mb-0 p-2 rounded-lg"
+          />
+          <p class="text-red-500 text-xs italic h-4 ml-2"></p>
+        </div>
+      </div>
+      <input
+        type="email"
+        placeholder="Email Address"
+        class="border m-2 w-full"
+      />
+      <input type="password" placeholder="Password" class="border m-2" />
+      <input type="password" placeholder="Verify Password" class="border m-2" />
+      <button class="border m-2 p-2 rounded-lg">Sign Up</button>
+      <p onClick={onSignInClick}>Already have an account? Click here</p>
+    </div>
   </div>
 );
 
@@ -25,13 +49,13 @@ const Authentication = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div>
+    <>
       {isLogin ? (
         <LoginComponent onSignUpClick={() => setIsLogin(false)} />
       ) : (
         <SignupComponent onSignInClick={() => setIsLogin(true)} />
       )}
-    </div>
+    </>
   );
 };
 
